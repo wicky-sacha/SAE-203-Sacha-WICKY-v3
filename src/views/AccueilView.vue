24df719f60f43435d11e5dd7dbab9aa1fc826da4
wicky-sacha <sacha.wicky@edu.univ-fcomte.fr>
2022-05-31 10:12:00 +0200

    video correction

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -14,7 +14,7 @@ import Navi from "../components/Navi.vue";
 import Bottom from "../components/Bottom.vue";
 import Affiche from "../components/Affiche.vue";
 import Caroussel from "../components/Caroussel.vue";
-import Video from "../components/VideoYT.vue";
+import Video from "../components/Video.vue";
 import CardBloc from "../components/CardBloc.vue";
 import fondImage from "../assets/fond.png?url";
 export default {
