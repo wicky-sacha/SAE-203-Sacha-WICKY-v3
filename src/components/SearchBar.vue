49078890c51f1839d7033f96e4ccfcfe0f0377e9
wicky-sacha <sacha.wicky@edu.univ-fcomte.fr>
2022-06-01 12:10:28 +0200

    search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,116 +1,13 @@
 <template>
-  <form>
-    <div class="flex">
-      <label for="search-dropdown" class="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Email</label>
-      <button
-        id="dropdown-button"
-        data-dropdown-toggle="dropdown"
-        class="
-          z-10
-          inline-flex
-          flex-shrink-0
-          items-center
-          rounded-l-lg
-          border border-gray-300
-          bg-gray-100
-          py-2.5
-          px-4
-          text-center text-sm
-          font-medium
-          text-gray-900
-          hover:bg-gray-200
-          focus:outline-none focus:ring-4 focus:ring-gray-100
-          dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-700
-        "
-        type="button"
-      >
-        Catégories
-        <svg class="ml-1 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fill-rule="evenodd"
-            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-          ></path>
+  <div class="flex items-center justify-center">
+    <div class="flex rounded border-2">
+      <input type="text" class="w-80 px-4 py-2" placeholder="Rechercher..." />
+      <button class="flex items-center justify-center border-l px-4">
+        <svg class="h-6 w-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
         </svg>
       </button>
-      <div
-        id="dropdown"
-        class="z-10 hidden w-44 divide-y divide-gray-100 rounded bg-white shadow dark:bg-gray-700"
-        data-popper-reference-hidden=""
-        data-popper-escaped=""
-        data-popper-placement="top"
-        style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(897px, 5637px, 0px)"
-      >
-        <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
-          <li>
-            <button type="button" class="inline-flex w-full py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-              Mockups
-            </button>
-          </li>
-          <li>
-            <button type="button" class="inline-flex w-full py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-              Templates
-            </button>
-          </li>
-          <li>
-            <button type="button" class="inline-flex w-full py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-              Design
-            </button>
-          </li>
-          <li>
-            <button type="button" class="inline-flex w-full py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-              Logos
-            </button>
-          </li>
-        </ul>
-      </div>
-      <div class="relative w-full">
-        <input
-          type="search"
-          id="search-dropdown"
-          class="
-            z-20
-            block
-            w-full
-            rounded-r-lg
-            border border-l-2 border-gray-300 border-l-gray-50
-            bg-gray-50
-            p-2.5
-            text-sm text-gray-900
-            focus:border-blue-500 focus:ring-blue-500
-            dark:border-gray-600
-            dark:border-l-gray-700
-            dark:bg-gray-100
-            dark:text-black
-            dark:placeholder-gray-400
-            dark:focus:border-blue-500
-          "
-          placeholder="Artiste, Scene, Date, Pays"
-          required
-        />
-        <button
-          type="submit"
-          class="
-            absolute
-            top-0
-            right-0
-            rounded-r-lg
-            border border-blue-700
-            bg-blue-400
-            p-2.5
-            text-sm
-            font-medium
-            text-white
-            hover:bg-blue-800
-            focus:outline-none focus:ring-4 focus:ring-blue-300
-            dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
-          "
-        >
-          <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-          </svg>
-        </button>
-      </div>
     </div>
-  </form>
+  </div>
 </template>
+
--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAErySkjkrQL1w3ZZ69hRoVDvfjTFs-Xhw",
+    authDomain: "sae-203-les-trois-cirques.firebaseapp.com",
+    projectId: "sae-203-les-trois-cirques",
+    storageBucket: "sae-203-les-trois-cirques.appspot.com",
+    messagingSenderId: "68664375850",
+    appId: "1:68664375850:web:e2908006dd2c45ae7fe153"
+};
+
+const appFirebase = initializeApp(firebaseConfig)
+
 const app = createApp(App)
 
 app.use(router)
