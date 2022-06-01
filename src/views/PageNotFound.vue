b84ba051ce86878b444381a0886e56c50fbbfa91
wicky-sacha <sacha.wicky@edu.univ-fcomte.fr>
2022-06-01 08:57:33 +0200

    page 404

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,6 +1,27 @@
 <template>
-  <main>
-    <h1>ERREUR 404 - PAGE INTROUVABLE</h1>
-    <p>Il semblerait que vous soyez égaré...</p>
+  <main class="w-fullh-full bg-cover bg-left bg-no-repeat" :style="`background-image: url('${fondImage}')`">
+    <Navi />
+    <div class="grid auto-rows-[10rem] grid-cols-1 content-center justify-items-center p-20">
+      <h1 class="text-2xl">ERREUR 404 - PAGE INTROUVABLE</h1>
+      <img class="text-center" src="../assets/logo.png" alt="" />
+      <p class="text-2xl">On dirait que vous vous êtes perdu ...</p>
+    </div>
+    <Bottom />
   </main>
 </template>
+
+<script>
+import Navi from "../components/Navi.vue";
+import Bottom from "../components/Bottom.vue";
+import fondImage from "../assets/fond.png?url";
+
+export default {
+  data: function () {
+    return {
+      fondImage,
+    };
+  },
+
+  components: { Navi, Bottom },
+};
+</script>
